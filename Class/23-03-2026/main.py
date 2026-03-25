76474a8b988789425313c4b0a2691a5ee4aac48a
sasaistomin <sashaistomin404@gmail.com>
2026-03-25 09:26:55 +0200

    init

---
+++ Class/23-03-2026/main.py
@@ -0,0 +1,76 @@
+# # def Hello():
+# #     print('Hello')
+
+# # Hello()
+
+# # def hello_name(name):
+# #     return f'Hello {name}'
+
+# # print(hello_name('Sasha'))
+
+# # def Text():
+# #     print(f" \"Don't let the noise of others' opinions \n\tdrown out your own inner voice.\" \n\t\t\t\tSteve Jobs")
+
+# # Text()
+
+# # def Line():
+# #     size = int(input('Enter size line: '))
+# #     sim = input('Enter simbol: ')
+# #     for i in range(size):
+# #         print(sim, end='')
+
+# # Line()
+
+# # def s(name):
+# #     return name
+# #     p('qw')
+
+# # print(s('ads'))
+
+
+# # def MaxNamrber(num1, num2, num3, num4):
+# #     if num1 > num2:
+# #         if num1 > num3:
+# #             if num1 > num4: 
+# #                 return num1
+# #     if num2 > num1: 
+# #         if num2 > num3: 
+# #             if num2> num4:
+# #                 return num2
+# #     if num3 > num1:
+# #         if num3 > num2:
+# #             if num3 > num4:
+# #                 return num3
+# #     if num4 > num1:
+# #         if num4 > num2:
+# #             if num4 > num3:
+# #                 return num4
+# #     if num1 == num2 == num3 == num4: 
+# #         return 'All number =='
+
+# # print(MaxNamrber(5, 5, 3, 2))
+
+# # def SimpleNum(num):
+# #     if num % 1 == 0 and num % num == 0 and num > 1:
+# #         return True
+# #     else:
+# #         return False
+# # print(SimpleNum(81))
+
+# from random import randint
+
+# def MaxArr(arr):
+#     max = arr[0]
+#     for i in arr:
+#         if i > max:
+#             max = i
+#     return max
+
+# arr = [randint(1, 20) for _ in range(10)]
+
+# print(arr)
+# print(MaxArr(arr))
+
+print("Мене\nзвати\nБонд.", end=" ")
+print("Джеймс Бонд.")
+ 
